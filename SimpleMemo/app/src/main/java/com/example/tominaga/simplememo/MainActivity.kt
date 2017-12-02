557a323179eb6c4a063b2466cac54cbc1eaffea2
tominagas <shing.toming@gmail.com>
2017-12-02 14:30:13 +0900

    機能ごとにメソッドわけ、fabをメンバ変数に変更

--- SimpleMemo/app/src/main/java/com/example/tominaga/simplememo/MainActivity.kt
+++ SimpleMemo/app/src/main/java/com/example/tominaga/simplememo/MainActivity.kt
@@ -37,6 +37,7 @@ class MainActivity : AppCompatActivity() {
 
     // 型のうしろに "?" をつけるとnull許容型として、nullで初期化することができる。
     var listView: ListView? = null
+    var fab: FloatingActionButton? = null
 
     // アプリ立ち上げ後、まずここの処理が走る
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,33 +51,91 @@ class MainActivity : AppCompatActivity() {
 
         // activity_main.xmlに含まれる、ツールバーをid指定して取ってきてインスタンス化
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar) // これでツールバーでの操作がActivityに影響を与えられるようになる
 
+        initFloatingActionButton()
+        initListView()
+    }
+
+    // ツールバー右上のオプションメニューを追加する処理。
+    // 今回はオプションメニュー使わないので、falseをreturn。
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return false
+    }
+
+    // オプションメニューのどれかをタップした時にここの処理が走る。
+    // 今回は使わない。
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        return if (id == R.id.action_settings) {
+            true
+        } else super.onOptionsItemSelected(item)
+    }
+
+    /**
+     * Activity作成時、フローティングアクションボタン（画面右下の丸いボタン）の初期化・設定を行う
+     */
+    private fun initFloatingActionButton() {
+        fab = findViewById(R.id.fab)
+
+        // フローティングアクションボタンに対して、タップ時の動作を設定。
+        fab?.setOnClickListener { view ->
+            val transaction = supportFragmentManager.beginTransaction()
+
+            if (supportFragmentManager.fragments.isEmpty()) {
+                // メモ入力画面に切り替え
+                (view as FloatingActionButton).setImageDrawable(applicationContext.getDrawable(R.drawable.ic_check))
+
+                val fragment = MemoDetailFragment()
+                transaction.add(R.id.fragment_container, fragment, fragmentTag)
+                transaction.commit()
+
+            } else {
+                // メモ一覧画面に切り替え
+                (view as FloatingActionButton).setImageDrawable(applicationContext.getDrawable(R.drawable.ic_add))
+
+                val fragment = supportFragmentManager.findFragmentByTag(fragmentTag) as MemoDetailFragment
+                // メモの更新位置を取得
+                val memoPosition = fragment.getMemoPosition()
+                // 更新位置がマイナスの場合は新規作成、あった場合はメモを更新する
+                if (memoPosition < 0){
+                    memos.add(fragment.getMemoDetailText())
+                } else {
+                    memos[memoPosition] = fragment.getMemoDetailText()
+                }
+
+                updateMemoList()
+                transaction.remove(fragment)
+                transaction.commit()
+            }
+        }
+    }
+
+    /**
+     * Activity作成時、リストビューの初期化・設定を行う
+     */
+    private fun initListView() {
         listView = findViewById(R.id.list_view)
-
-        // Activityに対してツールバーを設定する。これでツールバーでの操作がActivityに影響を与えられるようになる
-        setSupportActionBar(toolbar)
-
-        // activity_main.xmlに含まれる、フローティングアクションボタンをインスタンス化
-        val fab = findViewById<FloatingActionButton>(R.id.fab)
 
         // リストビューに表示されている項目をタップした時の動作
         listView?.setOnItemClickListener { parent, view, position, id ->
             val transaction = supportFragmentManager.beginTransaction()
 
             // メモ入力画面に切り替え
-            fab.setImageDrawable(applicationContext.getDrawable(R.drawable.ic_check))
+            fab?.setImageDrawable(applicationContext.getDrawable(R.drawable.ic_check))
 
             // データを渡す為のBundleを生成し、渡すデータを内包させる
             val bundle = Bundle()
             // 保存されているメモの情報をbundleに格納
-            bundle.putString(updateMemoKey, memos.get(position))
+            bundle.putString(updateMemoKey, memos[position])
             // タップされたメモの場所をbundleに格納
             bundle.putInt(updateMemoPositionKey, position)
 
             // Fragmentを生成し、setArgumentsで先ほどのbundleをセットする
             val fragment = MemoDetailFragment()
-            // bundleに格納したmemoの情報をfragmentに渡す
-            fragment.setArguments(bundle)
+            fragment.arguments = bundle
 
             transaction.add(R.id.fragment_container, fragment, fragmentTag)
             transaction.commit()
@@ -102,58 +161,13 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
-        // フローティングアクションボタンに対して、タップ時の動作を設定する。
-        fab.setOnClickListener { view ->
-            val transaction = supportFragmentManager.beginTransaction()
-
-            if (supportFragmentManager.fragments.isEmpty()) {
-                // メモ入力画面に切り替え
-                (view as FloatingActionButton).setImageDrawable(applicationContext.getDrawable(R.drawable.ic_check))
-
-                val fragment = MemoDetailFragment()
-                transaction.add(R.id.fragment_container, fragment, fragmentTag)
-                transaction.commit()
-
-            } else {
-                // メモ入力画面から一覧画面に切り替え
-                (view as FloatingActionButton).setImageDrawable(applicationContext.getDrawable(R.drawable.ic_add))
-
-                val fragment = supportFragmentManager.findFragmentByTag(fragmentTag) as MemoDetailFragment
-                // メモの更新位置を取得
-                val memoPosition = fragment.getMemoPosition()
-                // 更新位置がマイナスの場合は新規作成、あった場合はメモを更新する
-                if (memoPosition < 0){
-                    memos.add(fragment.getMemoDetailText())
-                } else {
-                    memos.set(memoPosition, fragment.getMemoDetailText())
-                }
-                updateMemoList()
-                transaction.remove(fragment)
-                transaction.commit()
-            }
-        }
-
         // activity_main.xmlのListViewをセットアップしていく
         setupListView()
     }
 
-    // ツールバー右上のオプションメニューを追加する処理。
-    // 今回はオプションメニュー使わないので、falseをreturn。
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return false
-    }
-
-    // オプションメニューのどれかをタップした時にここの処理が走る
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.itemId
-
-        return if (id == R.id.action_settings) {
-            true
-        } else super.onOptionsItemSelected(item)
-
-    }
-
+    /**
+     * リストビューに表示するデータをセット
+     */
     private fun setupListView() {
         // GsonライブラリがKotlin対応してないため、この1行が必要。正直おまじない程度な認識。
         val type = object : TypeToken<ArrayList<String>>(){}.type
@@ -167,18 +181,20 @@ class MainActivity : AppCompatActivity() {
             memos = Gson().fromJson(savedMemoListJson, type)
         }
 
-        // ListViewで実際に表示するデータ群を、このArrayAdapterで指定する。
+        // ListViewで実際に表示するデータを、このArrayAdapterで指定する。
         // ArrayAdapter<String> ＝ String（文字列）型のデータを持つAdapterである、というのを指定してあげる
-        // 第1引数: this = Contextを渡す。AppCompatActivityを継承しているMainActivityクラスなら「this」で渡すことができる
+        // 第1引数: Contextを渡す。AppCompatActivityを継承しているMainActivityクラスなら「this」で渡すことができる
         // 第2引数: リストの1つのレイアウトを指定。ここでは自分で作ったレイアウトを渡してるが、Androidプリセットのレイアウトも用意されている。
-        // 第3引数: memos = ListViewに表示するデータ群を指定。
+        // 第3引数: ListViewに表示するデータを指定。
         val adapter = ArrayAdapter<String>(this, R.layout.list_item, memos)
 
         // ListViewに、上で作ったAdapterをセット。これでメモ一覧が表示されるようになる
         listView?.adapter = adapter
     }
 
-    // アプリの情報保持領域に、現在のメモ一覧全てを保存・上書き。その後リストビューを更新する
+    /**
+     * アプリの情報保持領域に、現在のメモ一覧全てを保存・上書き。その後リストビューを更新する
+     */
     private fun updateMemoList() {
         val editor = sharedPreferences.edit()
         editor.putString(memoListTag, Gson().toJson(memos))
